d48e3cf1b3b541bc33a38f46fd8717e3c0df3f94
Vido <vidoseaver@gmail.com>
2017-01-06 11:28:26 -0700

    Working thru response

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,11 @@
 class SearchController < ApplicationController
   def index
     zipcode = params["zipcode"]
-    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zipcode},25))?format=json&show=storeId,storeType,name&pageSize=2&apiKey#{ENV['API_KEY']}")
+    response = Faraday.get("https://api.bestbuy.com/v1/stores(area(#{zipcode},25))?format=json&show=longName,city,distance,phone&pageSize=15&apiKey=#{ENV['API_KEY']}")
+    json = JSON.parse(response.body, symbolize_names:true)
     binding.pry
+    total = json[:total]
+
+
   end
 end
